7c45c2df02b560ca925798509baa4c15ad78d92f
segedu <seggedu20@gmail.com>
2021-11-07 12:02:45 +0200

    update port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express"),
-  app = express(),
-  PORT = 8080;
+  app = express();
+require("dotenv").config();
 
 app.get("*", (req, res) => {
   res.send("welcome");
 });
 
+PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
 });
