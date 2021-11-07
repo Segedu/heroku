de135b2defb68a71710bbf9657ebf3a58ec92032
segedu <seggedu20@gmail.com>
2021-11-07 11:12:43 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+  app = express(),
+  PORT = 8080;
+
+app.get("*", (req, res) => {
+  res.send("welcome");
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
