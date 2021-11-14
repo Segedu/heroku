1e4878e62e855928fd2da438b094f60d4bfaf566
segedu <seggedu20@gmail.com>
2021-11-14 19:09:58 +0200

    14.11.2021

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require("express"),
-  app = express();
+const express = require("express");
 require("dotenv").config();
+
+const app = express();
 
 app.get("*", (req, res) => {
   res.send("welcome");
 });
 
+const MONGO_URL = process.env.MONGO_URL;
 PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
